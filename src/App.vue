9e3b62036abe4454111a5a85f85099f60b4622da
valar <1848212420@qq.com>
2020-04-21 16:41:02 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- <Swiper></Swiper> -->
+    <Photos></Photos>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import Swiper from "./views/swiper"
+import Photos from "./views/Photos.vue"
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // Swiper.
+    Photos
   }
 }
 </script>
@@ -21,8 +23,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Photos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="hero">
+      <div class="hero-body">
+        <h2 class="title">游记</h2>
+        <div class="columns is-multiline">
+          <div
+            class="column is-3"
+            v-for="n in 6"
+            :key="n"
+          >
+            <figure class="image is-4by3">
+              <img
+                :src="require(`@/assets/girl${n}.jpg`)"
+                alt=""
+              >
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Photos",
+  data: () => ({})
+}
+</script>
